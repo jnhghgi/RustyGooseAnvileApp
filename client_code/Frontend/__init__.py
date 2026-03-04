5e854fe35b63af8261bc484f4e2e75bf45311f52
luis.birnbaumer@student.htl-rankweil.at <luis.birnbaumer@student.htl-rankweil.at>
2026-03-04 09:50:07 +0000

    Created Frontend.RowTemplate1 and Frontend.ItemTemplate1; Edited Backend and Frontend

--- client_code/Frontend/__init__.py
+++ client_code/Frontend/__init__.py
@@ -10,5 +10,7 @@ class Frontend(FrontendTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    return_value = anvil.server.call('query_database','SELECt name FROM gefaengnis')
+    return_value = [entry["gefaengnis_name"] for entry in return_value]
+    self.drop_down_gefaengnisliste.items = return_value
+    print(return_value)
--- server_code/Backend.py
+++ server_code/Backend.py
@@ -4,16 +4,39 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import anvil.sqlite
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
+# Verbindung zur Datenbank (Die Datei muss in Anvil hochgeladen sein)
+db_path = anvil.server.get_app_origin() + "/rust_final.db"
+
+
+@anvil.server.callable
+def get_team_stats():
+  conn = sqlite3.connect(db_path)
+  cursor = conn.cursor()
+  cursor.execute("""
+        SELECT Team.Name, COUNT(Player.SteamID) 
+        FROM Team LEFT JOIN Player ON Team.TeamID = Player.TeamID 
+        GROUP BY Team.Name
+    """)
+  return cursor.fetchall()
+
+
+@anvil.server.callable
+def get_resource_distribution():
+  conn = sqlite3.connect(db_path)
+  cursor = conn.cursor()
+  cursor.execute("""
+        SELECT Item.Name, SUM(Stores.amount) 
+        FROM Item JOIN Stores ON Item.ItemID = Stores.ItemID 
+        GROUP BY Item.Name
+    """)
+  return cursor.fetchall()
+
+@anvil.server.callable
+def get_all_players():
+  conn = sqlite3.connect(db_path)
+  cursor = conn.cursor()
+  cursor.execute("SELECT Name, Playtime FROM Player ORDER BY Playtime DESC")
+  return cursor.fetchall()
