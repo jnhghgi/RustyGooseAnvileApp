ecba9934342bc914c81e24082d79cdb5a69ef00d
luis.birnbaumer@student.htl-rankweil.at <luis.birnbaumer@student.htl-rankweil.at>
2026-03-12 11:27:43 +0000

    Edited Frontend.TeamForm

--- client_code/Frontend/TeamForm/__init__.py
+++ client_code/Frontend/TeamForm/__init__.py
@@ -9,10 +9,10 @@ from anvil.tables import app_tables
 class TeamForm(TeamFormTemplate):
   def __init__(self,row_Dict , **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties())
+    self.init_components(**properties
     
     # Any code you write here will run before the form opens.
-    return_value = anvil.server.call('get_Players_OnTeam(1)')
+    return_value = anvil.server.call('get_Players_OnTeam', row_Dict['TeamID'])
     return_value = [{"SteamID" : r[0], "Name": r[1], "Playtime": r[2]} for r in return_value]
     self.RustTeam_repeating_panle.items = return_value
     print(return_value)
